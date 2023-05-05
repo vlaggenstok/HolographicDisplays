52c00fab587240cc45febcdfe4f27980b28a2d9c
Ramon schipper <ramonschipper@hotmail.nl>
2023-05-05 19:24:09 +0200

    Added support for 1.19.3

---
+++ nms/v1_19_r3/src/main/java/me/filoghost/holographicdisplays/nms/v1_19_R3/InboundPacketHandler.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) filoghost and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package me.filoghost.holographicdisplays.nms.v1_19_R3;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import me.filoghost.fcommons.logging.Log;
+import me.filoghost.fcommons.reflection.ReflectField;
+import me.filoghost.holographicdisplays.nms.common.NMSErrors;
+import me.filoghost.holographicdisplays.nms.common.PacketListener;
+import net.minecraft.network.protocol.game.PacketPlayInUseEntity;
+import net.minecraft.world.EnumHand;
+import net.minecraft.world.phys.Vec3D;
+import org.bukkit.entity.Player;
+
+class InboundPacketHandler extends ChannelInboundHandlerAdapter {
+
+    public static final String HANDLER_NAME = "holographic_displays_listener";
+    private static final ReflectField<Integer> ENTITY_ID_FIELD = ReflectField.lookup(int.class, PacketPlayInUseEntity.class, "a");
+
+    private final Player player;
+    private final PacketListener packetListener;
+
+    InboundPacketHandler(Player player, PacketListener packetListener) {
+        this.player = player;
+        this.packetListener = packetListener;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext context, Object packetObject) throws Exception {
+        try {
+            if (packetObject instanceof PacketPlayInUseEntity) {
+                PacketPlayInUseEntity packet = (PacketPlayInUseEntity) packetObject;
+
+                int entityID = ENTITY_ID_FIELD.get(packet);
+                Action action = new Action();
+                packet.a(action);
+
+                if (action.isRightClick != null) {
+                    boolean cancel = packetListener.onAsyncEntityInteract(player, entityID, action.isRightClick);
+                    if (cancel) {
+                        return;
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            Log.warning(NMSErrors.EXCEPTION_ON_PACKET_READ, t);
+        }
+        super.channelRead(context, packetObject);
+    }
+
+    private static class Action implements PacketPlayInUseEntity.c {
+
+        Boolean isRightClick = null;
+
+        public void a(EnumHand var1) { // INTERACT
+            if (var1 == EnumHand.b) return; // OFF_HAND
+
+            this.isRightClick = true;
+        }
+
+        public void a(EnumHand var1, Vec3D var2) { // INTERACT_AT
+        }
+
+        public void a() { // ATTACK
+            this.isRightClick = false;
+        }
+    }
+
+}
